bb8e5fceac155e5efb957f63f55c3558a572bfcd
StephenHawking73 <dr-25@gmx.de>
2025-03-07 16:57:00 +0100

    SafeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View>
-    </View>
+    <SafeAreaView>
+      <Text>Tipper</Text>
+    </SafeAreaView>
   );
 }
