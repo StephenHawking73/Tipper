a240bbf8c9e22ca8b8935885be4aaab107250152
EnderGamerCode <endergamer654@gmail.com>
2025-03-08 14:10:35 +0100

    StyleSheet+TitleStyle

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,28 @@
 import { Text, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Slider, {SliderProps} from '@react-native-community/slider';
+import React from "react";
 
 export default function Index() {
   return (
     <SafeAreaView>
-      <Text>Tipper</Text>
+      <Text style={styles.title}>Tipper</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    marginLeft: 20,
+    marginTop: 75,
+    fontFamily: 'Madimi One',
+    fontSize: 64,
+    textAlign: "center",
+    textShadowColor: 'rgba(0, 0, 0, 0.35)',
+    textShadowOffset:{
+      height:7, 
+      width: 0
+    },
+    textShadowRadius: 4
+  }
+})
