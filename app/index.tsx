ea6abd0b1c6e880e3ae89948619ee5f1b8c291d6
StephenHawking73 <dr-25@gmx.de>
2025-03-09 13:30:56 +0100

    Added Greeting

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,52 @@
 import { Text, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Slider, {SliderProps} from '@react-native-community/slider';
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Index() {
+  const [greeting, setGreeting] = useState('Hallo');
+  useEffect(() => {
+    const getCurrentGreeting = () => {
+      const hour = new Date().getHours();
+
+      if (hour < 12) {
+        return 'Guten Morgen';
+      } else if (hour < 18) {
+        return 'Guten Tag';
+      } else{
+        return 'Guten Abend';
+      }
+    }
+    
+    setGreeting(getCurrentGreeting());
+  }, [])
+
   return (
     <SafeAreaView>
+      {/* Header */}
       <Text style={styles.title}>Tipper</Text>
+      <Text style={styles.greeting}>{greeting}! 👋</Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   title: {
-    marginLeft: 20,
-    marginTop: 75,
-    fontFamily: 'Madimi One',
+    marginTop: 30,
+    marginLeft: 45,
     fontSize: 64,
-    textAlign: "center",
-    textShadowColor: 'rgba(0, 0, 0, 0.35)',
+    fontFamily: "MadimiOne-Regular",
+    textShadowColor: '#9B9B9B', //use Hex-Code instead
     textShadowOffset:{
       height:7, 
       width: 0
     },
-    textShadowRadius: 4
-  }
+    textShadowRadius: 4,
+  },
+  greeting: {
+    marginTop: 10,
+    marginLeft: 45,
+    fontFamily: "MadimiOne-Regular",
+    fontSize: 20,
+  },
 })
