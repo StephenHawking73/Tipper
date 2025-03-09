bfa59962b73e5ffcc6c429b2f087e766bf8dac0a
StephenHawking73 <dr-25@gmx.de>
2025-03-09 13:24:03 +0100

    Load Fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
-import React from "react";
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { SplashScreen, Stack } from "expo-router";
+
+import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
+  //load Fonts for global use
+  const [fontsLoaded] = useFonts({
+    "MadimiOne-Regular": require("../assets/fonts/MadimiOne-Regular.ttf"),
+  })
+
+  useEffect(() => {
+    if (fontsLoaded){
+      console.log("Fonts Loaded");
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  
+  if (!fontsLoaded) return null;
+
+  
   return <Stack screenOptions={{headerShown: false}}/>;
 }
