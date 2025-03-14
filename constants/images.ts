87c6e5569da7494ab74eaa18abf1f5bcbe449432
StephenHawking73 <dr-25@gmx.de>
2025-03-14 23:44:43 +0100

    Fixed images.ts

--- constants/images.ts
+++ constants/images.ts
@@ -1,3 +1,5 @@
 export default {
     Credit_Card: require("../assets/images/Credit_Card.jpg"),
+    Payments: require("../assets/images/Payments.png"),
+    Wallet: require("../assets/images/Wallet.png"),
   };
